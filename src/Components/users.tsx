6d6d9be10145aa9e67a09b9c47a686af64502522
AnnAntonova2310 <anyashaida@mail.ru>
2022-10-05 23:46:12 +0500

    Button and condition

--- src/Components/users.tsx
+++ src/Components/users.tsx
@@ -3,14 +3,32 @@ import api from '../api'
 
 const Users = () => {
     const [users, setUsers] = useState(api.users.fetchAll())
-    const handlerDelete = () => {
-        console.log('Delete')
+    const handlerDelete = (userId: string) => {
+        let usersDelete = users.filter(u => u._id !== userId)
+        setUsers(usersDelete)
     }
+    let badge;
+    if (users.length === 0) {
+        badge = 'Никто не тусанёт с тобой сегодня'
+    } else if (users.length > 1 && users.length < 5) {
+        badge = `${users.length} человека тусанёт с тобой сегодня`
+    } else {
+        badge = `${users.length} человек тусанет с тобой сегодня`
+    }
+
 
     return (
         <>
-            <h1>Users</h1>
-            <table>
+            {/*/!*<h1>Никто не тусанёт с тобой сегодня </h1>*!/  л = 0*/}
+            {/*/!*<h1>1 человек тусанет с тобой сегодня </h1>*!/ л = 1, 5*/}
+            {/*/!*<h1>2 человека тусанут с тобой сегодня </h1>*!/  л = 2*/}
+
+            <h1>{badge}</h1>
+
+            {/*<h1>{ users.length === 0 ? 'Никто не тусанёт с тобой сегодня' : null }</h1>*/}
+
+
+            {users.length > 0 && <table>
                 <tr>
                     <th>№</th>
                     <th>Имя</th>
@@ -23,7 +41,7 @@ const Users = () => {
 
                     return (
                         <tr key={u._id}>
-                            <td>{i+1}</td>
+                            <td>{i + 1}</td>
                             <td>{u.name}</td>
                             <td>{
                                 u.qualities.map(q => q.name)
@@ -31,13 +49,13 @@ const Users = () => {
                             <td>{u.profession.name}</td>
                             <td>{u.completedMeetings}</td>
                             <td>{u.rate}</td>
-                            <td>{
-                                <button onClick={handlerDelete}>Delete</button>
-                            }</td>
+                            <td>
+                                <button onClick={() => handlerDelete(u._id)}>Delete</button>
+                            </td>
                         </tr>
-                        )
+                    )
                 })}
-            </table>
+            </table>}
         </>
     );
 };
