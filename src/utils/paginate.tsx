037ea859fb87c3cedb9b931b9663ad34b8e73164
AnnAntonova2310 <anyashaida@mail.ru>
2023-01-01 22:26:40 +0500

    initial commit

--- src/utils/paginate.tsx
+++
@@ -1,4 +0,0 @@
-export const paginate = (users: Array<any>, currentPage: number, pageSize: number)=> {
-    const startIndex = (currentPage-1)*pageSize
-    return [...users].splice(startIndex,pageSize)
-}
