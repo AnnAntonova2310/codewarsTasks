c92afc28c73433284ee0e27f63f56de6e9975bdc
AnnAntonova2310 <anyashaida@mail.ru>
2023-04-18 20:08:13 +0500

    remove_char

---
+++ remove_char.js
@@ -0,0 +1,11 @@
+function removeChar(str){
+    let newStr = ''
+    for(let i = 0; i < str.length; i++){
+        if(i !== 0 && i !== str.length -1) {
+            newStr += str[i]
+        }
+    }
+    return newStr;
+};
+
+removeChar('hello');
