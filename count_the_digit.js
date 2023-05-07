a6dcf4619456d0f684f7848eaa979dc6df4a5e75
AnnAntonova2310 <anyashaida@mail.ru>
2023-05-07 20:19:39 +0500

    count_the_digit

---
+++ count_the_digit.js
@@ -0,0 +1,10 @@
+function nbDig(n, d) {
+    let res=0;
+    for (let g=0;g<=n;g++){
+        let square=(g*g+"").split("");
+        square.forEach((s)=>s==d?res++:null)
+    }
+    return res;
+}
+
+nbDig();
